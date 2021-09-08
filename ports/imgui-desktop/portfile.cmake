803e93f4bb7017b452d01a1b86c6a096625f6481
Matt Haynie <mrhaynie@live.com>
2021-09-08 07:11:52 -0700

    add imgui-desktop

---
+++ ports/imgui-desktop/portfile.cmake
@@ -0,0 +1,24 @@
+set(VCPKG_USE_HEAD_VERSION ON)
+
+vcpkg_from_github(
+  OUT_SOURCE_PATH SOURCE_PATH
+  REPO PazerOP/imgui_desktop
+  HEAD_REF master
+)
+
+vcpkg_configure_cmake(
+	SOURCE_PATH "${SOURCE_PATH}"
+	PREFER_NINJA
+	# OPTIONS -DBUILD_SHARED_LIBS=ON;-DBUILD_TESTING=OFF
+)
+
+vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+file(
+	INSTALL "${SOURCE_PATH}/LICENSE"
+	DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+	RENAME copyright
+)
