47acc0035c141b9063bf9faa4693cb7b84179e8d
Matt Haynie <mrhaynie@live.com>
2021-09-08 09:05:06 -0700

    update imgui-cmake

--- ports/imgui-cmake/portfile.cmake
+++ ports/imgui-cmake/portfile.cmake
@@ -1,28 +1,35 @@
 set(VCPKG_USE_HEAD_VERSION ON)
 
-vcpkg_from_git(
-  OUT_SOURCE_PATH SOURCE_PATH
-  URL https://github.com/PazerOP/imgui_cmake.git
-  HEAD_REF main
-)
+# vcpkg_from_git(
+#   OUT_SOURCE_PATH SOURCE_PATH
+#   URL https://github.com/PazerOP/imgui_cmake.git
+#   HEAD_REF main
+# )
 
 find_program(GIT git)
-vcpkg_execute_required_process(
-	COMMAND "${GIT}" rev-parse FETCH_HEAD
-	WORKING_DIRECTORY "${SOURCE_PATH}"
-	LOGNAME imgui-cmake_rev-parse
-	OUTPUT_VARIABLE IMGUI_CMAKE_HEAD_REF
-)
-vcpkg_execute_required_process(
-	COMMAND "${GIT}" checkout --recurse-submodules ${IMGUI_CMAKE_HEAD_REF}
-	WORKING_DIRECTORY "${SOURCE_PATH}"
-	LOGNAME imgui-cmake_checkout
-)
+# vcpkg_execute_required_process(
+# 	COMMAND "${GIT}" rev-parse FETCH_HEAD
+# 	WORKING_DIRECTORY "${SOURCE_PATH}"
+# 	LOGNAME imgui-cmake_rev-parse
+# 	OUTPUT_VARIABLE IMGUI_CMAKE_HEAD_REF
+# )
+# vcpkg_execute_required_process(
+# 	COMMAND "${GIT}" checkout --recurse-submodules ${IMGUI_CMAKE_HEAD_REF}
+# 	WORKING_DIRECTORY "${SOURCE_PATH}"
+# 	LOGNAME imgui-cmake_checkout
+# )
 # vcpkg_execute_required_process(
 # 	COMMAND "${GIT}" submodule update --init --recursive
 # 	WORKING_DIRECTORY "${SOURCE_PATH}"
 # 	LOGNAME imgui-cmake_submodule_update
 # )
+set(SOURCE_PATH "${CURRENT_BUILDTREES_DIR}/src/head")
+vcpkg_execute_required_process(
+	COMMAND "${GIT}" clone https://github.com/PazerOP/imgui_cmake.git . --depth 1 --recurse-submodules
+	WORKING_DIRECTORY "${SOURCE_PATH}"
+	LOGNAME imgui-cmake_git_clone
+)
+
 
 vcpkg_configure_cmake(
 	SOURCE_PATH "${SOURCE_PATH}"
