be05f43de32509e929eb7e00f35c9343b579e0a9
Matt Haynie <mrhaynie@live.com>
2021-09-08 07:44:19 -0700

    add imgui-cmake...maybe

---
+++ ports/imgui-cmake/portfile.cmake
@@ -0,0 +1,29 @@
+set(VCPKG_USE_HEAD_VERSION ON)
+
+vcpkg_from_github(
+  OUT_SOURCE_PATH SOURCE_PATH
+  REPO PazerOP/imgui_cmake
+  HEAD_REF master
+)
+
+find_program(GIT git)
+vcpkg_execute_required_process(
+	COMMAND "${GIT}" init-submodules --update --recursive
+	WORKING_DIRECTORY "${SOURCE_PATH}"
+)
+
+vcpkg_configure_cmake(
+	SOURCE_PATH "${SOURCE_PATH}"
+	PREFER_NINJA
+)
+
+vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+file(
+	INSTALL "${SOURCE_PATH}/LICENSE"
+	DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+	RENAME copyright
+)
