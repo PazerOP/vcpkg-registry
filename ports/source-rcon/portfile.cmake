6b38de3337619f2f7909d14aa5790743c4a77790
Matt Haynie <mrhaynie@live.com>
2021-09-08 11:10:14 -0700

    no message

--- ports/source-rcon/portfile.cmake
+++ ports/source-rcon/portfile.cmake
@@ -10,7 +10,7 @@ vcpkg_cmake_configure(
 	SOURCE_PATH "${SOURCE_PATH}"
 )
 
-vcpkg_cmake_build()
+vcpkg_cmake_install()
 vcpkg_fixup_cmake_targets()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
