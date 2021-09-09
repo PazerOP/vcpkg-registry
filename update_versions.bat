4855652b7c771b1c5d344472529ce5edf787c3c0
Matt Haynie <mrhaynie@live.com>
2021-09-09 10:33:59 -0700

    Updated update_versions.bat to make it impossible to forget to commit stuff

--- update_versions.bat
+++ update_versions.bat
@@ -7,10 +7,15 @@ SETLOCAL
 	ECHO Formatting manifests...
 	vcpkg format-manifest --all %COMMON_ARGS%
 
+	ECHO Committing ports...
+	git add ports/*
+	git commit ports/* -m "Updated ports"
+
 	ECHO Updating versions...
 	vcpkg x-add-version --all --overwrite-version --verbose %COMMON_ARGS%
 
 	ECHO Committing versions...
+	git add versions/*
 	git commit versions/* -m "Updated versions"
 	git push
 
