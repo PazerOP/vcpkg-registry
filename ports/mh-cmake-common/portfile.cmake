b7669eeebe10de6bb8ea52894f12dfb270b11f69
Matt Haynie <mrhaynie@live.com>
2021-09-09 10:50:56 -0700

    Updated ports

--- ports/mh-cmake-common/portfile.cmake
+++ ports/mh-cmake-common/portfile.cmake
@@ -1,5 +1,6 @@
 file(INSTALL
 	"${CMAKE_CURRENT_LIST_DIR}/vcpkg-port-config.cmake"
+	"${CMAKE_CURRENT_LIST_DIR}/mh-cmake-common-config.cmake"
     "${CMAKE_CURRENT_LIST_DIR}/mh-CheckCoroutineSupport.cmake"
     "${CMAKE_CURRENT_LIST_DIR}/mh-CheckCoroutineSupport.cpp"
     "${CMAKE_CURRENT_LIST_DIR}/mh-CheckUnicodeSupport.cmake"
