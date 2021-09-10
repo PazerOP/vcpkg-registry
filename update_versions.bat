c287744e8ff7b9f4a00e685953a9e4f6e818bd8f
Matt Haynie <mrhaynie@live.com>
2021-09-10 15:32:08 -0700

    remove old version files

--- update_versions.bat
+++ update_versions.bat
@@ -5,21 +5,21 @@ SETLOCAL
 	SET COMMON_ARGS=--x-builtin-ports-root=../my_vcpkg_repo/ports/ --x-builtin-registry-versions-dir=../my_vcpkg_repo/versions/
 
 	ECHO Formatting manifests...
-	vcpkg format-manifest --all %COMMON_ARGS%
+	vcpkg format-manifest --all %COMMON_ARGS% || EXIT /b %ERRORLEVEL%
 
 	ECHO Committing ports...
-	git add ports/*
-	git commit ports/* -m "Updated ports"
+	git add ports/* || EXIT /b %ERRORLEVEL%
+	git commit ports/* -m "Updated ports" || EXIT /b %ERRORLEVEL%
 
 	ECHO Updating versions...
-	vcpkg x-add-version --all --overwrite-version --verbose %COMMON_ARGS%
+	vcpkg x-add-version --all --overwrite-version --verbose %COMMON_ARGS% || EXIT /b %ERRORLEVEL%
 
 	ECHO Committing versions...
-	git add versions/*
-	git commit versions/* -m "Updated versions"
-	git push
+	git add versions/* || EXIT /b %ERRORLEVEL%
+	git commit versions/* -m "Updated versions" || EXIT /b %ERRORLEVEL%
+	git push || EXIT /b %ERRORLEVEL%
 
 	ECHO Latest revision:
-	git rev-parse HEAD
+	git rev-parse HEAD || EXIT /b %ERRORLEVEL%
 
 ENDLOCAL
